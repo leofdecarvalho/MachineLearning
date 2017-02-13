028d720ceca0212315b4738f9c925555bd1a01f9
Leonardo <leofdecarvalho@gmail.com>
2017-02-12 23:53:47 -0200

    Encode data in R and Python

--- 1. Data Preprocessing/data_preprocessing_template.py
+++ 1. Data Preprocessing/data_preprocessing_template.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+#print all data
 np.set_printoptions(threshold=np.inf)
 
 #Import the dataset
@@ -16,3 +17,14 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis=0)
 imputer = imputer.fit(x[:, 1:3]) 
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#Enconde categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotenconder = OneHotEncoder(categorical_features=[0])
+x = onehotenconder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_x.fit_transform(y)
+
